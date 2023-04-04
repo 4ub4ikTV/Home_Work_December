f6515b924dc215df56a726108406ebdc9d79df7f
4ub4ik <svyantkoorest@gmail.com>
2023-04-04 16:20:59 +0300

    update les1 TypeScript

--- TypeScript/les1/app.ts
+++ TypeScript/les1/app.ts
@@ -35,7 +35,85 @@
 //     }
 // }
 // }
-//
+import axios from "axios";
+
+interface LaunchSite {
+    site_name_long: string
+}
+
+interface Links {
+    article_link: string
+    video_link: string
+}
+
+interface Core {
+    flight: number
+    core: {
+        reuse_count: number
+        status: string
+    }
+}
+
+interface Payload {
+    payload_type: string
+    payload_mass_kg: number
+    payload_mass_lbs: number
+}
+
+interface Rocket {
+    rocket_name: string
+    first_stage: {
+        cores: Core[]
+    }
+    second_stage: {
+        payloads: Payload[]
+    }
+}
+
+interface Mission {
+    mission_name: string
+    launch_date_local: string
+    launch_site: LaunchSite
+    links: Links
+    rocket: Rocket
+}
+
+const mission: Mission = {
+    mission_name: "Starlink-15 (v1.0)",
+    launch_date_local: "2020-10-24T11:31:00-04:00",
+    launch_site: {
+        site_name_long: "Cape Canaveral Air Force Station Space Launch Complex 40",
+    },
+    links: {
+        article_link: "http://some.com",
+        video_link: "https://youtu/J442-ti-Dhg",
+    },
+    rocket: {
+        rocket_name: "Falcon 9",
+        first_stage: {
+            cores: [
+                {
+                    flight: 7,
+                    core: {
+                        reuse_count: 6,
+                        status: "unknown",
+                    },
+                },
+            ],
+        },
+        second_stage: {
+            payloads: [
+                {
+                    payload_type: "Satellite",
+                    payload_mass_kg: 15400,
+                    payload_mass_lbs: 33951.2,
+                },
+            ],
+        },
+    },
+};
+
+
 // ---------------------------------------------------------------------------—————————
 // 2) протипізувати функції:
 //
@@ -60,3 +138,28 @@
 // console.log(sum(1, 2));
 // showSum(2,3)
 // incAge(user, 2)
+
+
+const user: { name: string, age: number, gender: string } = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
+
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: typeof user, inc: number): typeof user {
+    someUser.age += inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2, 3)
+incAge(user, 2)
+
